04a8a48d9cb0cb4834fb18a4a8cf186791a89909
Alex Szabo <87szabo@gmail.com>
2015-05-19 14:28:46 -0700

    Sets up redis for session storage Sets up RedisToGo addon for heroku deployment Adds production error handling

--- app.js
+++ app.js
@@ -10,14 +10,32 @@ var express = require('express'),
 // Dotenv for secret env variables
 var dotenv = require('dotenv').load();
 
-
 // Express.js Middleware
-var session = require('express-session'),
-  logger = require('morgan'),
-  errorHandler = require('errorhandler'),
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  var redisAuth = redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
+
+var logger = require('morgan'),
   cookieParser = require('cookie-parser'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override');
+
+var errorHandler = require('errorhandler');
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+} else if (process.env.NODE_ENV === 'production') {
+  app.use(errorHandler());
+}
 
 var app = express();
 app.locals.appTitle = 'sweet-blog';
@@ -34,12 +52,25 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Express midleware config
+// Express middleware config
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser(process.env.PARSER_SECRET));
-app.use(session({secret: process.env.SESSION_SECRET}));
+// in production
+app.use(session({ 
+  store: new RedisStore({ 
+    host: rtg.hostname, port: rtg.port, db: redisAuth[0], pass: redisAuth[1] }),
+  secret: process.env.SESSION_SECRET
+}));
+// in development     
+// app.use(session({ 
+//  store: new RedisStore({
+//    host: "127.0.0.1",
+//    port: "6379",
+//    db: 1 }),
+//  secret: process.env.SESSION_SECRET
+// }));
 app.use(methodOverride());
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -56,13 +87,9 @@ var authorize = function(req, res, next) {
   if (req.session && req.session.admin)
     return next();
   else
-    return res.send(401);
+    return res.sendStatus(401);
 };
 
-// use ErrorHandler in Dev
-if ('development' == app.get('env')) {
-  app.use(errorHandler());
-}
 
 // Pages/routes
 app.get('/', routes.index);
@@ -82,7 +109,7 @@ app.put('/api/articles/:id', routes.article.edit);
 app.del('/api/articles/:id', routes.article.delete);
 
 app.all('*', function(req, res) {
-  res.send(404);
+  res.sendStatus(404);
 })
 
 var server = http.createServer(app);
