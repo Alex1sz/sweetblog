100bf5b7d127e7144af02876e57b1bd4d86b1766
Alex Szabo <87szabo@gmail.com>
2015-05-19 17:37:27 -0700

    Updates errorhandler midware

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'development') {
     showStack: true
   }));
 } else if (process.env.NODE_ENV === 'production') {
-  app.use(express.errorhandler());
+  app.use(errorhandler());
 }
 
 var app = express();
